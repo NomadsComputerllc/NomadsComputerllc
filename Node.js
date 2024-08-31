6ea3326a45d0233e7a20aec7566d09462dc20988
NomadsComputerllc <Owners@nomadscomputerllc.com>
2024-08-31 02:08:14 -0700

    Update Node.js
    Run actions/setup-node@v4
      with:
        node-version: 20.x
        cache: npm
        always-auth: false
        check-latest: false
        token: ***
      env:
        AZURE_WEBAPP_NAME: your-app-name
        AZURE_WEBAPP_PACKAGE_PATH: .
        NODE_VERSION: 20.x
    Found in cache @ /opt/hostedtoolcache/node/20.17.0/x64
    Environment details
      node: v20.17.0
      npm: 10.8.2
      yarn: 1.22.22
    /opt/hostedtoolcache/node/20.17.0/x64/bin/npm config get cache
    /home/runner/.npm

--- Node.js
+++ Node.js
@@ -1,25 +1,5 @@
 
 
-Run actions/setup-node@v4
-  with:
-    node-version: 20.x
-    cache: npm
-    always-auth: false
-    check-latest: false
-    token: ***
-  env:
-    AZURE_WEBAPP_NAME: your-app-name
-    AZURE_WEBAPP_PACKAGE_PATH: .
-    NODE_VERSION: 20.x
-Found in cache @ /opt/hostedtoolcache/node/20.17.0/x64
-Environment details
-  node: v20.17.0
-  npm: 10.8.2
-  yarn: 1.22.22
-/opt/hostedtoolcache/node/20.17.0/x64/bin/npm config get cache
-/home/runner/.npm
-
-
 Post job cleanup.
 /usr/bin/git version
 git version 2.46.0
